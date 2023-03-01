95326b973ddbbbd0a299132776e28537287b0b05
anuvindvannery <johnd3006@gmail.com>
2023-03-01 14:02:50 +0530

    Revert "commit"
    This reverts commit bc3b8774a35b4cfc416aee07f0496149ed3fa4a7.

--- tripping/tripping/Login.aspx.cs
+++ tripping/tripping/Login.aspx.cs
@@ -65,22 +65,10 @@ namespace tripping
             //if (Buttonsignup.ValidationGroup == "a" && Page.IsValid == true)
             //{
                 Response.Redirect("signup.aspx");
-
-            
             //}
-        }
-
-        
-        //protected void Buttonout_Click(object sender, EventArgs e)
-        //{
-        //    Session.Abandon();
-        //    Session.Remove("UserId");
-        //    Session.Remove("Role");
-        //    Response.Redirect("signup.aspx");
-            
         }
     }
 
-
+}
 
 
